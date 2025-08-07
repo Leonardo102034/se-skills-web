5b4bd00fe8b3468b6dba4e669d0d51b940807e8c
Zidany Palomino <zidanypalomino2@gmail.com>
2025-08-07 13:20:17 -0500

    new changes for fisrt commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://IngZidany.github.io',
-  base: '/se-web-skills',
+  // base: '/se-web-skills', // Comentado para desarrollo local
   vite: {
     plugins: [tailwindcss()]
   }
